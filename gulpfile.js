8a4c548bb73ce823b17e40f6b305acd6564ad7d6
Brian Tanner <brian@tanner.io>
2016-12-31 11:35:16 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const cssPaths = {
+	src: './css/**/*.scss',
+	dest: './css',
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', ['sass'], () => {
+  gulp.watch(cssPaths.src, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('sass', () =>
+  gulp.src(cssPaths.src)
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest(cssPaths.dest)));
